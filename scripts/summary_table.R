460be27be9718dfe38f83638b2217eba296b9f49
assafvayner <assafvayner@gmail.com>
2020-05-17 14:45:32 -0700

    summary table and create index.Rmd

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -1,1 +1,15 @@
-# Assigned to Assaf
+library(dplyr)
+
+video_games <- read.csv("./data/Video_Games_Sales_as_at_22_Dec_2016.csv")
+video_games <- video_games[-1, ]
+  
+publisher_table <- video_games %>%
+  filter(Platform %in% c("PS3", "PS4", "X360", "XOne", "Wii")) %>%
+  group_by(Publisher) %>%
+  summarise(NA_Sales = sum(NA_Sales),
+            EU_Sales = sum(EU_Sales),
+            JP_Sales = sum(JP_Sales),
+            Other_Sales = sum(Other_Sales),
+            Global_Sales = sum(Global_Sales)) %>%
+  filter(Global_Sales > mean(Global_Sales) + sd(Global_Sales)) %>%
+  arrange(-Global_Sales)
