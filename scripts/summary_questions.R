ea88d0060cb82e6e13e3092f7bb7775d5600c37d
assafvayner <assafvayner@gmail.com>
2020-05-19 00:04:42 -0700

    genres and publishers change to top 3 vectors

--- scripts/summary_questions.R
+++ scripts/summary_questions.R
@@ -1,23 +1,25 @@
-# Assigned to Maya
-library("dplyr")
+library(dplyr)
+library(stringr)
 
 steam <- read.csv("./data/steam.csv", stringsAsFactors = FALSE)
 videogames <- read.csv("./data/video_games.csv", stringsAsFactors = FALSE)
 videogames_2016 <- read.csv("./data/Video_Games_Sales_as_at_22_Dec_2016.csv", stringsAsFactors = FALSE)
+videogames_2016 <- videogames_2016[-1, ] # remove outlier wii sports
 
 #Which publisher sold the most games in North America, Europe, Japan, and globally?
 # changed to publisher bc too many holes in dev column
-top_publisher <- function(region_sales){
+top3_publishers <- function(region_sales){
   videogames_2016 %>%
   group_by(Publisher) %>%
   summarise(num_sales = sum(!!as.name(region_sales))) %>%
-  filter(num_sales == max(num_sales)) %>%
+  arrange(-num_sales) %>%
+  slice(1:3) %>%
   pull(Publisher)
 }
-na_publisher <- top_publisher("NA_Sales")
-jp_publisher <- top_publisher("JP_Sales")
-eu_publisher <- top_publisher("EU_Sales")
-global_publisher <- top_publisher("Global_Sales")
+na_publisher <- top3_publishers("NA_Sales")
+jp_publisher <- top3_publishers("JP_Sales")
+eu_publisher <- top3_publishers("EU_Sales")
+global_publisher <- top3_publishers("Global_Sales")
 
 # What are the top 3 game genres in a given region based on sales?
 top3_genres <- function(region_sales) {
@@ -25,8 +27,9 @@ top3_genres <- function(region_sales) {
   group_by(Genre)%>% 
   summarise(NA_genre = sum(!!as.name(region_sales))) %>%
   arrange(-NA_genre) %>%
-  top_n(3)
-}  
+  slice(1:3) %>%
+  pull(Genre)
+}
 na_genres <- top3_genres("NA_Sales")
 jp_genres <- top3_genres("JP_Sales")
 eu_genres <- top3_genres("EU_Sales")
@@ -38,5 +41,4 @@ eu_genres <- top3_genres("EU_Sales")
 
 
 #Are there more occurences of people buying multiplayer or single player games?
-
 
