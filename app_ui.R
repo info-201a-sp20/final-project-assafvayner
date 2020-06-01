fe8aa2ffc750bb6fe47194b044b4d6dde9d5fafe
assafvayner <assafvayner@gmail.com>
2020-06-01 10:45:01 -0700

    css changes

--- app_ui.R
+++ app_ui.R
@@ -1,38 +1,49 @@
-#TESTTESTESTSTSTS
+library(plotly)
 library(shiny)
+library(dplyr)
 
+games <- c(
+  "Grand Theft Auto V", "Call of Duty: Black Ops 3", "Mario Kart 7",
+  "Halo 3", "Minecraft"
+)
 
-games <- c("Grand Theft Auto V", "Call of Duty: Black Ops 3", "Mario Kart 7",
-           "Halo 3", "Minecraft")
-           
-game_choices <- selectInput("pie_choice", label = "Game Series Choice",
-                            games, games[1])
+game_choices <- selectInput("pie_choice",
+  label = "Game Series Choice",
+  games, games[1]
+)
 
-game_types <-  video_games_2016 %>%
+game_types <- video_games_2016 %>%
   pull(Genre) %>%
   unique()
 game_types <- game_types[-13]
 
-genre_choices <- selectInput("bar_choice", label = "Genre Choice",
-                             game_types, game_types[1])
+genre_choices <- selectInput("bar_choice",
+  label = "Genre Choice",
+  game_types, game_types[1]
+)
 
-sales_lab <- c("Global Sales", "North America Sales", "Europe Sales", "Japan Sales",
-                "Other Sales")
+sales_lab <- c(
+  "Global Sales", "North America Sales", "Europe Sales", "Japan Sales",
+  "Other Sales"
+)
 
 x_axis_choice <- selectInput("x", "X axis Choice", sales_lab[-1], sales_lab[2])
 y_axis_choice <- selectInput("y", "Y axis Choice", sales_lab[-1], sales_lab[3])
 z_axis_choice <- selectInput("z", "Z axis Choice", sales_lab[-1], sales_lab[4])
-color_by <- selectInput("color_by", "Color by variable", sales_lab,
-                        sales_lab[1])
+color_by <- selectInput(
+  "color_by", "Color by variable", sales_lab,
+  sales_lab[1]
+)
 sd_choice <- numericInput("sd", "Standard Deviations Above the Mean", value = 6)
 
 ui <- fluidPage(
-  titlePanel("Title"),
-  
+  includeCSS("style.css"),
+  h1("Title"),
+
   tabsetPanel(
     tabPanel(
       "Intro",
-      
+      h2("introd"),
       p("intro info")
     ),
     tabPanel(
@@ -42,17 +53,22 @@ ui <- fluidPage(
           game_choices
         ),
         mainPanel(
-          h1("Pie Chart"),
+          h2("Pie Chart"),
           plotOutput("pie"),
-          p("The following", em("pie charts"), "show a comparison between the amount of overall", strong("sales"),
+          p(
+            "The following", em("pie charts"), "show a comparison between the
+            amount of overall", strong("sales"),
             "of games that are released on multiple platforms.
-            The pie chart is split into different slices resembling", strong("sales
-            on each platform"), "the game is sold. This chart is included to clearly show 
-            the", em("sales among platforms"), "for specific games to clear inquiry regarding the many inputs of a certain game.")
+            The pie chart is split into different slices resembling",
+            strong("sales on each platform"), "the game is sold. This chart is
+            included to clearly show the", em("sales among platforms"),
+            "for specific games to clear inquiry regarding the many inputs of a
+            certain game."
+          )
         )
-        )
-      ),
-     
+      )
+    ),
+
     tabPanel(
       "Visualization of Game Genres",
       sidebarLayout(
@@ -60,17 +76,21 @@ ui <- fluidPage(
           genre_choices
         ),
         mainPanel(
-          h1("Barchart"),
+          h2("Barchart"),
           plotOutput("bar"),
-          p("The following", em("barcharts"), "look at different genres to see which", em("games and publishers sold the most globally."), 
+          p(
+            "The following", em("barcharts"), "look at different genres to see
+            which", em("games and publishers sold the most globally."),
             "This barchart is included because
-            it is clear to see", strong("which games performed the most and which publisher produced the games."), "The usage of color
-            to", em("differentiate"), "publishers also adds to the clarity of the chart. Above are the barcharts of the", em("12"),
-            "genres, ranging from Racing to Strategy."),
-        )
+            it is clear to see", strong("which games performed the most and
+            which publisher produced the games."), "The usage of color
+            to", em("differentiate"), "publishers also adds to the clarity of
+            the chart. Above are the barcharts of the", em("12"),
+            "genres, ranging from Racing to Strategy."
+          ),
         )
       )
-    ,
+    ),
     tabPanel(
       "Visualization of Game Sales in Different Regions",
       sidebarLayout(
@@ -82,15 +102,16 @@ ui <- fluidPage(
           sd_choice
         ),
         mainPanel(
-          h1("3D Scatterplot"),
+          h2("3D Scatterplot"),
           plotlyOutput("Three_D"),
-          p("The following scatterplot is interactive to help
+          p(
+            "The following scatterplot is interactive to help
            see how well each game performed in specific regions and globally.
-           For the axis, you can change the the different options to be" ,
-            strong("North American, Japan, European"),  "and" ,
+           For the axis, you can change the the different options to be",
+            strong("North America, Japan, Europe"), "and",
             strong("Other sales"),
             ", while having the color scale to change with any of the listen
-            options about including", strong("Global sales"),". There is also
+            options about including", strong("Global sales"), ". There is also
             a number input for how many", strong("Standard Deviations"),
             "above the mean to sort out the global sales in order for the user
             to see results depending on how well the game did globally.
@@ -113,15 +134,15 @@ ui <- fluidPage(
             genres of popular games are also diverse being Platform, Puzzle,
             Shooter, and Role-playing so that means there is variety when
             it comes to popular games and not one genre is dominating the
-            market."),
-
+            market."
+          ),
         )
       )
     ),
     tabPanel(
       "summary",
+      h2("summary end"),
       p("summary")
     )
   )
 )
-
